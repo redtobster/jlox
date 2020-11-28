39ed95610224255c8cb7de3d6fbd856983244649
Toby Limanto <tobylimanto@gmail.com>
2020-11-28 13:58:08 +0800

    added abstract syntax tree class generator

---
+++ tool/GenerateAst.java
@@ -0,0 +1,94 @@
+package com.craftinginterpreters.tool;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
+
+/*
+ * This would be a java cmd tool that will generate Expr.java
+ *
+ * This class is a tool for us to generate base class for expressions.
+ * Then for each kind of expression (unary or binary or grouping),
+ * we create a subclass containing fields for their corresponding non terminals.
+ *
+ * so for example, if you want to access the second operand of a unary expression,
+ * you would get a compile error. This is because unary expression would only have
+ * one operand.
+ */
+public class GenerateAst {
+  public static void main(String[] args) throws IOException {
+    if (args.length != 1) {
+		System.err.println("Usage: generate_ast <output directory>");
+		System.exit(64);
+	}
+	String outputDir = args[0];
+	defineAst(outputDir, "Expr", Arrays.asList(
+		/*
+		 * left hand side (name of the class) then
+		 * semi-colon then
+		 * right hand side (list of the fields)
+		 */
+		"Binary   : Expr Left, Token operator, Expr right",
+		"Grouping : Expr expression",
+		"Literal  : Object value",
+		"Unary    : Token operator, Expr right"
+    ));
+  }
+
+  private static void defineAst( String outputDir, String baseName, List<String> types)
+    throws IOException {
+    String path = outputDir + "/" + baseName + ".java";
+	PrintWriter writer = new PrintWriter(path, "UTF-8");
+
+  /*
+   * defineAst:
+   * output base Expr class (first thing to do).
+   */
+    writer.println("package com.craftinginterpreters.lox;");
+	writer.println();
+	writer.println("import java.util.List;");
+	writer.println();
+	writer.println("abstract class " + baseName + " {"); // not hard coding because there will be separate family of classes later on
+
+	// AST Classes. These are each defined in the base class
+	for (String type : types){
+		String className = type.split(":")[0].trim(); //left hand side
+		String fields = type.split(":")[1].trim(); //right hand side
+		defineType(writer, baseName, className, fields); // defineType is written just below
+	}
+
+	// never forget the closing curly brackets
+	writer.println("}");
+    writer.close();
+
+  }
+
+  /*
+   * defineType HERE!!
+   */
+  private static void defineType( PrintWriter writer, String baseName, String className, String fieldList ){
+    writer.println("  static class " + className + " extends " + baseName + " {");
+
+	// Constructor for the class
+	writer.println("    " + className + "(" + fieldList + ") {");
+
+	// Store parameters in fields.
+	String[] fields = fieldList.split(", ");
+	for (String field: fields) {
+		String name = field.split(" ")[1];
+		writer.println("    this." + name + " = " + name + ";");
+	}
+
+	writer.println("    }");
+
+	// Fields
+	writer.println();
+	for ( String field : fields) {
+		writer.println("    final " + field + ";");
+	}
+
+	writer.println("  }");
+  }
+
+}
