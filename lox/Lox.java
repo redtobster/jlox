e6044eb8e3034af2455cf8d5a8ecaffffe504e31
Toby Limanto <tobylimanto@gmail.com>
2020-12-14 00:42:44 +0800

    interpreter class, added runtime error handling

--- lox/Lox.java
+++ lox/Lox.java
@@ -9,7 +9,9 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+  private static final Interpreter interpreter = new Interpreter();
   static boolean hadError = false;
+  static boolean hadRuntimeError = false;
   public static void main(String[] args) throws IOException {
     if (args.length > 1){
 		System.out.println("Usage: jlox [script]");
@@ -47,6 +49,7 @@ public class Lox {
   private static void run(String source) {
     // Indicate an error in the exit code.
     if (hadError) System.exit(65);
+	if (hadRuntimeError) System.exit(70);
     Scanner scanner = new Scanner(source);
 	List<Token> tokens = scanner.scanTokens();
 
@@ -57,7 +60,7 @@ public class Lox {
 	// Stop when syntax error is detected
 	if (hadError) return;
 
-	System.out.println(new AstPrinter().print(expression));
+	interpreter.interpret(expression);
   }
 
   /*
@@ -86,5 +89,11 @@ public class Lox {
 		  report(token.line, " at '" + token.lexeme + "'", message);
 		}
 	}
+
+  static void runtimeError(RuntimeError error) {
+    System.err.println(error.getMessage() + "\n[line " + error.token.line + "]");
+	hadRuntimeError = true;
+
+  }
 
 }
